caad98c59d3739131f353e2ae056969b3cf7a312
Marco Tan <marco.tan.200405@gmail.com>
2020-09-01 11:56:18 -0400

    :sparkles: Split main.cpp into multiple files.

---
+++ src/initialize.cpp
@@ -0,0 +1,26 @@
+/* Discobots 1104A comp code.
+ * Marco Tan, Neil Sachdeva
+ * 
+ * Code for initialization goes here.
+ */
+
+#include "main.h"
+
+/* disabled function */
+void disabled()
+{
+
+}
+
+/* comp init function */
+void competition_initialize()
+{
+
+}
+
+/* initialize function */
+void initialize()
+{
+    // TODO: create auton selector
+    // TODO: add necessary initialization
+}
---
+++ src/opcontrol.cpp
@@ -0,0 +1,13 @@
+/* Discobots 1104A comp code.
+ * Marco Tan, Neil Sachdeva
+ * 
+ * Code for driver control goes here.
+ */
+
+#include "main.h"
+
+/* driver control function */
+void opcontrol()
+{
+    // TODO: add tasks for driver control related functions.
+}
