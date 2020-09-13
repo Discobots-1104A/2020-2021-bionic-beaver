d81472b379b92e10b82f97f14cf1872333eb175c
Marco Tan <marco.tan.200405@gmail.com>
2020-09-13 13:00:31 -0400

    :sparkles: Add auto routine prototypes.

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -5,8 +5,42 @@
 
 #include "main.h"
 
+//> Forward Declarations of Autos <//
+auto red_auto() -> void;
+auto blu_auto() -> void;
+auto skl_auto() -> void;
+
 //> Autonomous Function <//
 void autonomous()
 {
-    
+    switch (kMisc::au_Selected_Auto)
+    {
+    case kMisc::k_Auto_Select::RED:
+        red_auto();
+        break;
+    case kMisc::k_Auto_Select::BLUE:
+        blu_auto();
+        break;
+    case kMisc::k_Auto_Select::SKILLS:
+        skl_auto();
+        break;
+    }
+}
+
+//> Red Auto <//
+auto red_auto() -> void
+{
+
+}
+
+//> Blue Auto <//
+auto blu_auto() -> void
+{
+
+}
+
+//> Skills Auto<//
+auto skl_auto() -> void
+{
+
 }
