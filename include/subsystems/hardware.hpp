edcea18b985b2b3254d31e176f96fba251756a87
Marco Tan <marco.tan.200405@gmail.com>
2020-09-26 13:44:51 -0400

    :sparkles: Add hardware source files.

---
+++ include/subsystems/hardware.hpp
@@ -0,0 +1,13 @@
+//* Discobots 1104A comp code.
+//* Marco Tan, Neil Sachdeva, Dev Patel
+//*
+//* File Created: 2020-09-26
+//* Desc: Hardware class declarations.
+
+
+//* Headers
+
+
+
+
+//* Declarations
---
+++ src/subsystems/hardware.cpp
@@ -0,0 +1,14 @@
+//* Discobots 1104A comp code.
+//* Marco Tan, Neil Sachdeva, Dev Patel
+//*
+//* File Created: 2020-09-26
+//* Desc: Hardware class definitions.
+
+
+//* Headers
+
+// Main header.
+#include "main.h"
+
+
+//* Definitions
