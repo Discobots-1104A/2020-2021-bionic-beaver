3be9e8d08deebc66eedefae0c66367788227da7b
Marco Tan <marco.tan.200405@gmail.com>
2020-09-26 13:22:31 -0400

    :truck: Move main callbacks to individual files.

---
+++ src/initialize.cpp
@@ -0,0 +1,32 @@
+//* Discobots 1104A comp code.
+//* Marco Tan, Neil Sachdeva, Dev Patel
+//*
+//* File Created: 2020-09-26
+//* Desc: Main initialization code.
+
+
+//* Headers
+
+// Main header.
+#include "main.h"
+
+
+//* Functions
+
+// Disabled state callback.
+void disabled()
+{
+
+}
+
+// Competition initialization callback.
+void competition_initialize()
+{
+
+}
+
+// Main initialization callback.
+void initialize()
+{
+
+}
---
+++ src/opcontrol.cpp
@@ -0,0 +1,20 @@
+//* Discobots 1104A comp code.
+//* Marco Tan, Neil Sachdeva, Dev Patel
+//*
+//* File Created: 2020-09-26
+//* Desc: Main operator control code.
+
+
+//* Headers
+
+// Main header.
+#include "main.h"
+
+
+//* Functions
+
+// Main operator control callback.
+void opcontrol()
+{
+
+}
