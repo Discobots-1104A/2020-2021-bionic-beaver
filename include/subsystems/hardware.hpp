ed1edcd4f833f5a708ee9ba59c20d7968fcdeb8d
Marco Tan <marco.tan.200405@gmail.com>
2020-09-26 19:52:59 -0400

    :sparkles: Add hardware structs for later.

--- include/subsystems/hardware.hpp
+++ include/subsystems/hardware.hpp
@@ -6,8 +6,42 @@
 
 
 //* Headers
-
-
+#include "api.h"    // PROS API header.
 
 
 //* Declarations
+
+namespace k_Hardware
+{
+    //* Constants
+
+
+    //* User-defined types
+
+    /// struct - drive Ports.
+    //! In order of: LF, LB, RF, RB.
+    struct Drive_Ports
+    {
+        int pt_LF;  // Drive port, left front.
+        int pt_LB;  // Drive port, left back.
+        int pt_RF;  // Drive port, right font.
+        int pt_RB;  // Drive port, right back.
+    };
+
+    /// struct - Conveyor ports.
+    //! In order of: bottom, top.
+    struct Conveyor_Ports
+    {
+        int pt_CB;  // Conveyor port, bottom.
+        int pt_CT;  // Conveyor port, top.
+    };
+
+    /// struct - Intake ports.
+    //! In order of: left, right.
+    struct Intake_Ports
+    {
+        int pt_IL;  // Intake port, left.
+        int pt_IR;  // Intake port, right.
+    };
+
+}
