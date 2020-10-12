1d47097f496d92cae9db05cd148d8265b109ef22
Marco Tan <marco.tan.200405@gmail.com>
2020-10-12 12:55:21 -0400

    :alembic: Add more auto test code for later.

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -18,7 +18,15 @@ a_PID pid{a_PID_Gains{0.1, 0, 0.5, 5, 0, 2, 0.25}};
 void autonomous()
 {
     h_obj_sensors.reset();
-    pid.reset();
-    pid.set_target(a_Ticks{720}).drive();
+    pid.reset().set_target(a_Ticks{720}).drive();
     pros::delay(1000);
+    pid.set_target(a_Ticks{-720}).drive();
+    pros::delay(2000);
+    pid.set_target(a_Degrees{90}).drive();
+    pros::delay(1000);
+    pid.set_target(a_Degrees{0}).drive();
+    pros::delay(1000);
+    pid.set_target(a_Degrees{270}).drive();
+    pros::delay(1000);
+    pid.set_target(a_Degrees{0}).drive();
 }
