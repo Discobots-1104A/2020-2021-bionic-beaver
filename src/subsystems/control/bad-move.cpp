290ae4e9653afa310769913dc08b7345cfccc29d
Marco Tan <marco.tan.200405@gmail.com>
2020-10-16 17:22:06 -0400

    :bulb: Update simple movement comments.

--- src/subsystems/control/bad-move.cpp
+++ src/subsystems/control/bad-move.cpp
@@ -20,14 +20,20 @@ a_Bad_Move::a_Bad_Move()
 /// \param dist Distance to move to.
 void a_Bad_Move::move_str(double dist)
 {
+    // Set a fixed speed based on the direction of the target.
     h_obj_chassis->drive_vel(std::copysign(50, dist));
 
+    // Loop until OCRs tell us that we've reach the target.
     while (std::fabs(dist - h_obj_sensors->get_enc(h_Encoder_IDs::AVG_SIDES)) > k_Auto::a_def_ocr_tick_range)
     {
+        // Delay because the OCRs can't read any faster than this.
         pros::delay(k_Hardware::h_max_readtime);
     }
 
+    // Stop the motors.
     h_obj_chassis->drive_vel();
+
+    // Reset OCR values.
     h_obj_sensors->reset();
 }
 
@@ -35,15 +41,23 @@ void a_Bad_Move::move_str(double dist)
 /// \param head Heading to move to.
 void a_Bad_Move::move_pnt_trn(double head)
 {
+    // Calculate the shortest angle towards the target from current heading.
     double diff { fmod( (head - h_obj_sensors->get_heading() + 180.0), 360.0) - 180 };
     double theta { ( (diff < -180) ? diff + 360 : diff ) };
 
+    // Set a fixed speed based on the shortest angle to the target from current heading.
     h_obj_chassis->drive_vel(std::copysign(50, theta), std::copysign(50, -theta));
+
+    // Loop until IMU tells us that we've reached the target.
     while (std::fabs(head - h_obj_sensors->get_heading()) > k_Auto::a_def_imu_head_range)
     {
+        // Delay because the IMU can't read any faster than this.
         pros::delay(k_Hardware::h_max_readtime);
     }
 
+    // Stop the motors.
     h_obj_chassis->drive_vel();
+
+    // Reset OCR values.
     h_obj_sensors->reset();
 }
