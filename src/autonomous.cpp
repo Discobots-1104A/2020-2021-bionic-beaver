d82078e0becae545a45652cd712de12cf6784189
Marco Tan <marco.tan.200405@gmail.com>
2020-10-18 17:20:32 -0400

    :bulb: Add some auto comments.

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -36,11 +36,13 @@ void skills()
 // Main autonomous control callback.
 void autonomous()
 {
+    // Create new a_PID object into heap memory.
     a_obj_pid = new a_PID{a_PID_Gains{
         k_Auto::a_def_kP, k_Auto::a_def_kI, k_Auto::a_def_kD, 
         k_Hardware::h_max_readtime, k_Auto::a_def_integ_windup, 
         k_Auto::a_def_ocr_tick_range, k_Auto::a_def_imu_head_range}};
     
+    // Run each auto routine based on selected auto.
     switch (a_routine)
     {
     case a_Autonomous_Routine::RED:
