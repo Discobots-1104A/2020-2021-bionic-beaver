b97c67dd0ef1344abb2e78fe231a377d83b6d4c5
Marco Tan <marco.tan.200405@gmail.com>
2020-10-28 16:10:40 -0400

    :sparkles: Add base chassis class files

---
+++ include/hardware/chassis.hpp
@@ -0,0 +1,15 @@
+//* 2020-2021-bionic-beaver
+//* 
+//* Author(s):      Dev Patel   <hello.devpatel@gmail.com>
+//*                 Marco Tan   <marco.tan.200405@gmail.com>
+//*             Neil Sachdeva   <Wiserlightning090@gmail.com>
+//*
+//* Desc:       Chassis declarations.
+
+#ifndef CHASSIS_HPP
+#define CHASSIS_HPP
+
+#include "api.h"
+
+
+#endif  // CHASSIS_HPP
