0f922c18d8496f3f213a0c301b5455b553d586d7
Marco Tan <marco.tan.200405@gmail.com>
2020-10-28 11:12:22 -0400

    :bulb: File comments

--- src/init.cpp
+++ src/init.cpp
@@ -1,3 +1,11 @@
+//* 2020-2021-bionic-beaver
+//* 
+//* Author(s):      Dev Patel   <hello.devpatel@gmail.com>
+//*                 Marco Tan   <marco.tan.200405@gmail.com>
+//*             Neil Sachdeva   <Wiserlightning090@gmail.com>
+//*
+//* Desc:       Initialization code.
+
 #include "main.h"
 
 
