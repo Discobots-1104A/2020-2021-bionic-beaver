adbe85d5757c23941029d291385bc1294e4a456f
Marco Tan <marco.tan.200405@gmail.com>
2020-10-29 18:45:38 -0400

    :sparkles: Add intake declrs

--- include/hardware/intake.hpp
+++ include/hardware/intake.hpp
@@ -13,4 +13,43 @@
 
 #include "api.h"
 
+/// Intake control class
+class h_intake
+{
+public:
+    h_intake(
+        std::uint8_t pt_motor_l,
+        std::uint8_t pt_motor_r,
+        pros::motor_gearset_e cart = pros::E_MOTOR_GEARSET_06, 
+        pros::motor_brake_mode_e brake = pros::E_MOTOR_BRAKE_HOLD, 
+        pros::motor_encoder_units_e enc_unit = pros::E_MOTOR_ENCODER_COUNTS,
+        bool rv_l = false, bool rv_r = true
+    );
+
+    // Movement
+    void move_vel(int vel);
+    void move_vel(int vel_l, int vel_r);
+
+    // Config
+    h_intake& set_brake_mode(pros::motor_brake_mode_e brake_mode);
+
+private:
+    // Port numbers
+    std::uint8_t    m_pt_motor_l,
+                    m_pt_motor_r;
+
+    // Motor is reversed
+    bool    m_pt_motor_l,
+            m_pt_motor_r;
+
+    // Gearset
+    pros::motor_gearset_e m_cart;
+
+    // Brake mode
+    pros::motor_brake_mode_e m_brake;
+
+    // Encoder units
+    pros::motor_encoder_units_e m_enc_unit;
+};
+
 #endif  // INTAKE_HPP
