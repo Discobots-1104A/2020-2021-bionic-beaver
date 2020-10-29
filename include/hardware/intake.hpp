6fef155d88c78249cf9459ac0e0091120f0bc599
Marco Tan <marco.tan.200405@gmail.com>
2020-10-29 17:36:49 -0400

    :sparkles: Add files for intake class declrs and defs

---
+++ include/hardware/intake.hpp
@@ -0,0 +1,16 @@
+//* 2020-2021-bionic-beaver
+//* 
+//* Author(s):      Dev Patel   <hello.devpatel@gmail.com>
+//*                 Marco Tan   <marco.tan.200405@gmail.com>
+//*             Neil Sachdeva   <Wiserlightning090@gmail.com>
+//*
+//* Desc:       Intake declarations.
+
+//! Try to prefix the main declarations with "h_"
+
+#ifndef INTAKE_HPP
+#define INTAKE_HPP
+
+#include "api.h"
+
+#endif  // INTAKE_HPP
