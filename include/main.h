a9470a45af8c92cfc56fc631c1b5866bd4ce9585
Marco Tan <marco.tan.200405@gmail.com>
2020-11-01 17:00:21 -0500

    :tada: Prep chassis files for later

--- include/main.h
+++ include/main.h
@@ -42,6 +42,9 @@
 //#include "okapi/api.hpp"
 //#include "pros/api_legacy.h"
 
+//* Hardware
+#include "subsystems/hardware/chassis.hpp"
+
 /**
  * If you find doing pros::Motor() to be tedious and you'd prefer just to do
  * Motor, you can use the namespace with the following commented out line.
---
+++ src/subsystems/hardware/chassis.cpp
@@ -0,0 +1,12 @@
+//* 2020-2021-bionic-beaver
+//* 
+//* Author(s):      Dev Patel   <hello.devpatel@gmail.com>
+//*                 Marco Tan   <marco.tan.200405@gmail.com>
+//*             Neil Sachdeva   <Wiserlightning090@gmail.com>
+//*
+//* Desc:       Chassis class definitions
+
+#include "main.h"
+
+
+
