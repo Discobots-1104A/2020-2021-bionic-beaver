9e02a60bb28a6ba5a469c93f6e0a7c375755e010
Marco Tan <marco.tan.200405@gmail.com>
2020-11-09 16:39:10 -0500

    :construction: Add files for miscellaneous utils

---
+++ include/subsystems/misc/utils.hpp
@@ -0,0 +1,17 @@
+//* 2020-2021-bionic-beaver
+//* 
+//* Author(s):      Dev Patel   <hello.devpatel@gmail.com>
+//*                 Marco Tan   <marco.tan.200405@gmail.com>
+//*             Neil Sachdeva   <Wiserlightning090@gmail.com>
+//*
+//* Desc:       Miscellaneous utilities
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include "api.h"
+
+
+//
+
+#endif  // UTILS_HPP
