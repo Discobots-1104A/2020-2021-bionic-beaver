0ed9ef6626162a7fb50541c55bd7c3e4c252d6a3
Marco Tan <marco.tan.200405@gmail.com>
2020-11-09 11:26:22 -0500

    :construction: Working on odom

--- include/subsystems/control/odometry.hpp
+++ include/subsystems/control/odometry.hpp
@@ -12,6 +12,89 @@
 #include "api.h"
 
 
-//
+/// Tower center coords and optimal heading
+/// Values based on Desmos field abstraction
+struct c_Goal_Center_Coords
+{
+    double x;       // X coord in inches
+    double y;       // Y coord in inches
+    double head;    // Optimal heading in degrees
+};
+
+/// Ball center coords at start
+/// Values based on Desmos field abstraction
+struct c_Ball_Center_Coords
+{
+                        double  x;      // X coord in inches
+                        double  y;      // Y coord in inches
+    h_Sensors_Vision_Signatures colour; // Ball colour
+};
+
+/// Robot starting position based on turning center of robot
+/// Values will deviate, but will also try to be corrected for
+struct c_Robot_Starting_Pos_Coords
+{
+    double x;       // X coord in inches
+    double y;       // Y coord in inches
+    double head;    // Starting heading in degrees
+};
+
+/// Coordinates and optimal headings of all tower centers
+struct c_All_Goal_Coords
+{
+    c_Goal_Center_Coords m_goal_a;  // Goal A
+    c_Goal_Center_Coords m_goal_b;  // Goal B
+    c_Goal_Center_Coords m_goal_c;  // Goal C
+    c_Goal_Center_Coords m_goal_d;  // Goal D
+    c_Goal_Center_Coords m_goal_e;  // Goal E
+    c_Goal_Center_Coords m_goal_f;  // Goal F
+    c_Goal_Center_Coords m_goal_g;  // Goal G
+    c_Goal_Center_Coords m_goal_h;  // Goal H
+    c_Goal_Center_Coords m_goal_I;  // Goal I
+};
+
+/// Starting coordinates of skills setup balls
+/// Expect a 0.5 deviation from these values
+struct c_Skills_Setup_Startup_Coords
+{
+    c_Ball_Center_Coords m_ball_a_b;    // Ball between Goals A-B
+    c_Ball_Center_Coords m_ball_b_c;    // Ball between Goals B-C
+    c_Ball_Center_Coords m_ball_b;      // Ball in front of Goal B
+    c_Ball_Center_Coords m_ball_a_e;    // Ball between Goals A-E
+    c_Ball_Center_Coords m_ball_c_f;    // Ball between Goals C-F
+    c_Ball_Center_Coords m_ball_e_t;    // Ball top of Goal E
+    c_Ball_Center_Coords m_ball_e_l;    // Ball left of Goal E
+    c_Ball_Center_Coords m_ball_e_r;    // Ball right of Goal E
+    c_Ball_Center_Coords m_ball_e_b;    // Ball bottom of Goal E
+    c_Ball_Center_Coords m_ball_e_g;    // Ball between Goals E-G
+    c_Ball_Center_Coords m_ball_f_i;    // Ball between Goals F-I
+    c_Ball_Center_Coords m_ball_h;      // Ball in front of Goal H
+    c_Ball_Center_Coords m_ball_g_h;    // Ball between Goals G-H
+    c_Ball_Center_Coords m_ball_h_i;    // Ball between Goals H-I
+};
+
+/// Starting coordinates of live comp setup balls
+/// Expect a 0.5 deviation from these values
+struct c_Live_Comp_Setup_Startup_Coords
+{
+    c_Ball_Center_Coords m_ball_a;      // Ball in front of Goal A
+    c_Ball_Center_Coords m_ball_b;      // Ball in front of Goal B
+    c_Ball_Center_Coords m_ball_c;      // Ball in front of Goal C
+    c_Ball_Center_Coords m_ball_e_t;    // Ball top of Goal E
+    c_Ball_Center_Coords m_ball_e_l;    // Ball left of Goal E
+    c_Ball_Center_Coords m_ball_e_r;    // Ball right of Goal E
+    c_Ball_Center_Coords m_ball_e_b;    // Ball bottom of Goal E
+    c_Ball_Center_Coords m_ball_h;      // Ball in front of Goal H
+    c_Ball_Center_Coords m_ball_g;      // Ball in front of Goal G
+    c_Ball_Center_Coords m_ball_i;      // Ball in front of Goal I
+};
+
+/// Struct of all robot starting positions
+struct c_Robot_Starting_Positions
+{
+    c_Robot_Starting_Pos_Coords m_live_start_red;   // Starting position for live red
+    c_Robot_Starting_Pos_Coords m_live_start_blue;  // Starting position for live blue
+    c_Robot_Starting_Pos_Coords m_skills;           // Starting position for skills
+};
 
 #endif  // ODOMETRY_HPP
