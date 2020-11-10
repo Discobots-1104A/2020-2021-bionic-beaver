74281c54f186f168c0f48f874569a62281194054
Marco Tan <marco.tan.200405@gmail.com>
2020-11-10 12:48:12 -0500

    :sparkles: Add basic driver functionality (movement)

--- src/teleop.cpp
+++ src/teleop.cpp
@@ -9,7 +9,29 @@
 #include "main.h"
 
 
+//* Check deadzone
+auto check_deadzone 
+{[](int var){
+    return (std::abs(var) > g_Constants::g_teleop_deadzone) ? var : 0;
+}};
+
+//* Chassis control task
+void chassis_control()
+{
+    while (!(pros::competition::is_autonomous() || pros::competition::is_disabled()))
+    {
+        int pow  {check_deadzone(h_obj_controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y))};
+        int turn {check_deadzone(h_obj_controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X))};
+
+        h_obj_chassis->move(pow + turn, pow - turn);
+
+        pros::delay(10);
+    }
+}
+
 void opcontrol() 
 {
+    c_obj_odom->start_odom();
 
+    pros::Task t_chassis {chassis_control};
 }
