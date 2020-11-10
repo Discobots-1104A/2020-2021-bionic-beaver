184651efcadf78dd39ac2d31bd3acc11e993101a
Marco Tan <marco.tan.200405@gmail.com>
2020-11-10 18:32:04 -0500

    :alembic: Still troubleshooting odom

--- src/subsystems/control/odometry.cpp
+++ src/subsystems/control/odometry.cpp
@@ -107,10 +107,9 @@ pros::c::imu_accel_s_t c_Odometry::get_accel_vals(void) {return m_current_accel_
 //* Private methods *//
 
 /// Filter values
-double c_Odometry::m_filter_values(double current_val, double last_val)
+double c_Odometry::m_filter_values(double current_val)
 {
-    double filter = current_val - last_val;
-    return filter;
+    return std::roundf(current_val * 10.0) / 10.0;
 }
 
 /// Updates odometry values.
@@ -120,15 +119,15 @@ void c_Odometry::m_update_func(void)
     {
         // Getting rotation, pitch, and roll
         m_current_rotation = m_sensors_obj->imu_get_rotation();
-        m_filtered_rotation += m_filter_values(m_current_rotation, m_last_rotation);
+        m_filtered_rotation += m_filter_values(m_current_rotation);
         m_last_rotation = m_current_rotation;
 
         m_current_pitch = m_sensors_obj->imu_get_pitch();
-        m_filtered_pitch += m_filter_values(m_current_pitch, m_last_pitch);
+        m_filtered_pitch += m_filter_values(m_current_pitch);
         m_last_pitch = m_current_pitch;
 
         m_current_roll = m_sensors_obj->imu_get_roll();
-        m_filtered_roll += m_filter_values(m_current_roll, m_last_roll);
+        m_filtered_roll += m_filter_values(m_current_roll);
         m_last_pitch = m_current_pitch;
 
         // Getting gyroscopic values.
